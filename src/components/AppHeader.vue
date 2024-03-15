6e17f675d1a883f23b695d105bc833258fbcf3b9
scarlett-troncoso <scar.labussieri@gmail.com>
2024-03-15 11:55:46 +0100

    import store and add in items

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,12 +1,36 @@
 <script>
+import { store } from '../store';
+
 export default {
-    name: 'AppHeader'
+    name: 'AppHeader',
+
+    data () {
+        return{
+            store,
+        }
+    },
+
+    methods: {
+        filterResults() {
+            //&query=coraline
+            const url_movie = `${this.store.base_api_url}&query=${this.store.searchMovieandSerie}`;
+            const url_serie = `${this.store.base_api_url_serie}&query=${this.store.searchMovieandSerie}`;
+            //console.log(url_movie);
+            console.log(this.store.searchMovieandSerie);
+
+            this.store.getInfoCards(url_movie);
+            this.store.getInfoCardsSerie(url_serie);
+        },
+    },
 }
 </script>
 
 <template>
     <header class="container">
-        Header
+        <div>
+            <input type="text" placeholder="Search Movie or Serie" v-model="store.searchMovieandSerie">
+            <button @click="filterResults">Search</button>
+        </div>
     </header>
 </template>
 
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,5 @@
 <script>
-import axios from 'axios';
+import { store } from '../store';
 import AppStars from './AppStars.vue';
 
 export default {
@@ -10,76 +10,12 @@ export default {
 
     data() {
         return {
-            base_api_url: 'https://api.themoviedb.org/3/search/movie?api_key=d7aac37017d487828e63f03c5d26591d', //&query=coraline
-            base_api_url_serie: 'https://api.themoviedb.org/3/search/tv?api_key=d7aac37017d487828e63f03c5d26591d',
-            results: [],
-            results_serie: {},
-            error: false,
-            searchMovieandSerie: '',
+           store, 
+
             languages: 'https://www.unknown.nu/flags/images/', //uk-100   //https://www.unknown.nu/flags/
             url_img: 'https://image.tmdb.org/t/p/w200/',
             url_no_img: 'https://static.thenounproject.com/png/340719-200.png',
-            
         }
-    },
-
-    
-
-    methods: {
-        getInfoCards(url) {
-            axios
-                .get(url).then((response) => {
-                    //console.log(response);
-                    //console.log(response.data.results);
-                    this.results = response.data.results
-                    console.log(this.results);
-                    // console.log(this.results[0].title);
-                    // this.error = false;
-
-                    /*
-                    this.results.forEach((result, index) => {
-                        this.vote.push(this.vote_star(result.vote_average))
-                    });*/
-                })
-                .catch((error) => {
-                    console.error(error);
-                    this.error = error.message;
-                })
-        },
-
-        getInfoCardsSerie(url) {
-            axios
-                .get(url).then((response) => {
-                    this.results_serie = response.data.results
-
-                    for (let index = 0; index < this.results_serie.length; index++) { //for per ciclare dentro l'array di serie
-                        const list_serie = this.results_serie[index];
-                        this.results.push(list_serie) //questa soluzione fa aparire la lista delle serie sotto della lista dei film, in tanto cosi
-                        // this.vote.push(this.vote_star(list_serie.vote_average))
-                        //questa e una posibile PER STAMPARE IN PAGINA PRIMA LE SERIE in caso che queste abbiano IL NOME PIU ESATTO DA QUELLO CERCATO
-
-                        //return this.searchMovieandSerie === list_serie.name || list_serie.original_name ? this.results.unshift(list_serie) : this.results.push(list_serie)
-                    }
-                    console.log(this.results);
-                    
-                    console.log('CONSOLE VOTE', this.vote);
-                })
-                .catch((error) => {
-                    console.error(error);
-                    this.error = error.message;
-                })
-        },
-
-        filterResults() {
-            //&query=coraline
-            const url_movie = `${this.base_api_url}&query=${this.searchMovieandSerie}`;
-            const url_serie = `${this.base_api_url_serie}&query=${this.searchMovieandSerie}`;
-            //console.log(url_movie);
-            console.log(this.searchMovieandSerie);
-
-            this.getInfoCards(url_movie);
-            this.getInfoCardsSerie(url_serie);
-        },
     },
 
     /*
@@ -92,26 +28,25 @@ export default {
 
 <template>
     <main class="container">
-        <div>
-            <input type="text" placeholder="Search Movie or Serie" v-model="searchMovieandSerie">
-            <button @click="filterResults">Search</button>
-        </div>
+        
         <div class="row">
-            <ul v-for="result in results" :key="result.id + '_result'" class="col-5 card">
+            <ul v-for="result in store.results" :key="result.id + '_result'" class="col-5 card">
                 <div class="cont-img">
                     <img :src="result.poster_path === null ? url_no_img : url_img + result.poster_path" alt="poster:">
                 </div>
-                <li>Title: {{ result.title || result.name }} </li>
-                <li>Original Title: {{ result.original_title || result.original_name }}</li>
-                <li>Language: {{ result.original_language }}
+                <div class="details_film">
+                    <li>Title: {{ result.title || result.name }} </li>
+                    <li>Original Title: {{ result.original_title || result.original_name }}</li>
+                    <li>Language: {{ result.original_language }}
                     <img :src="languages + result.original_language + '-100'" alt="bandiera" class="flag"> <!--ua.png-->
-                </li>
+                    </li>
                 <!--<li> Vote: {{ vote_star(result.vote_average) }}</li> -->
-                <AppStars :vote_average="result.vote_average"></AppStars>
-                
+                    <AppStars :vote_average="result.vote_average"></AppStars>
+                    <li>Overview: {{ result.overview }}</li>
+                </div>        
             </ul>
         </div>
-        <div v-if="error">{{ error }}</div>
+        <div v-if="store.error">{{ store.error }}</div>
     </main>
 </template>
 
@@ -131,30 +66,5 @@ export default {
         width: 100%;
     }
 }
-
-.stars_outer {
-position: relative;
-display: inline-block;
-    > i {
-        color: #fff8;
-        /*-webkit-text-stroke: 1px #908f8f;
-        /*-webkit-text-stroke: 1px transparent;*/
-    }
-
-}
-
-.stars_inner {
-position: absolute;
-top: 0;
-left: 0;
-
-overflow: hidden;
-
->i {
-    color: gold;
-}
-}
-
-
 
 </style>
